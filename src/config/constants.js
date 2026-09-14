03f91d5ef47fa32dd31ff5fe238b7db5425509cd
Melina <mel.chana20@gmail.com>
2026-09-13 20:01:18 -0400

    Agregar configuracion del scraper

---
+++ src/config/constants.js
@@ -0,0 +1,10 @@
+const SCRAPER_CONFIG = {
+    navigationTimeout: 12000,
+    waitAfterLoad: 1500,
+    outputIndent: 2,
+    maxDescriptionLength: 5000
+};
+
+module.exports = {
+    SCRAPER_CONFIG
+};
